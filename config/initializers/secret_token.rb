64ed0fe278832ce8125aa660081a1c2f53303435
Dennis Acuna <dacuna30@academic.rrc.ca>
2014-04-09 21:53:47 -0500

    initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TireStore::Application.config.secret_key_base = 'a8760d3366377e3cb074ea5365af956a3e8c34452aa254011db551b2675c6d9b5e917b8f2a0e931c47e798ee5b3b05064f9e06c40d43516662db53c80dca0892'
